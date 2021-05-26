a7075a3b78473a61eecf5c6256b139c57167b174
alexuribarri <alex@uribarri.es>
2021-05-27 01:14:20 +0200

    final commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import './styles.css';
-import './js/1'
-import './js/2'
+
+import * as one from './js/1';
+import * as two from './js/2';
+import * as three from './js/3';
+
+const button1 = document.querySelector('#workOne');
+const button2 = document.querySelector('#workTwo');
+const button3 = document.querySelector('#workThree');
+
+button1.addEventListener('click', one.importWorkOne);
+button2.addEventListener('click', two.importWorkTwo);
+button3.addEventListener('click', three.importWorkThree);
--- src/js/2.js
+++ src/js/2.js
@@ -1,37 +1,31 @@
-//Перепиши функцию toggleUserState() так, чтобы она не использовала callback-функцию callback, 
+//Перепиши функцию toggleUserState() так, чтобы она не использовала callback-функцию callback,
 //а принимала всего два параметра allUsers и userName и возвращала промис.
 
-
 const users = [
-    { name: 'Mango', active: true },
-    { name: 'Poly', active: false },
-    { name: 'Ajax', active: true },
-    { name: 'Lux', active: false },
-  ];
-  
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
 
+const toggleUserState = (allUsers, userName) => {
+  return new Promise(resolve => {
+    const updatedUsers = allUsers.map(user =>
+      user.name === userName ? { ...user, active: !user.active } : user,
+    );
+    resolve(updatedUsers);
+  });
+};
 
+const logger = updatedUsers => console.table(updatedUsers);
 
-  const toggleUserState = (allUsers, userName) => {
-    return new Promise (resolve => {
-      const updatedUsers = allUsers.map(user =>
-        user.name === userName ? { ...user, active: !user.active } : user,)
-        resolve(updatedUsers)
-      
-    })
+/*
+ * Должно работать так
+ */
 
-
-  };
-
-
-  
-  const logger = updatedUsers => console.table(updatedUsers);
-  
-
-  
-  /*
-   * Должно работать так
-   */
+const importWorkTwo = () => {
   toggleUserState(users, 'Mango').then(logger);
   toggleUserState(users, 'Lux').then(logger);
+};
 
+export { importWorkTwo };
